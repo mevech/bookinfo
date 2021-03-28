981a8c5347f72a117a2153f8eb614e1b3995b45a
mevech <eve.m.chen@gmail.com>
2021-03-28 10:12:48 +0200

    local ci

--- src/build-services.sh
+++ src/build-services.sh
@@ -1,73 +1,89 @@
 #!/bin/bash
-#
-# Copyright Istio Authors
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+export PREFIX=$( aws sts get-caller-identity | jq .Account -r ).dkr.ecr.eu-north-1.amazonaws.com
 
-set -o errexit
+function build_and_push {
+  for SERVICE in productpage details reviews ratings mysql mongodb
+  do
+    REPO_NAME="${PREFIX}/examples-bookinfo-${SERVICE}"
+    echo ${REPO_NAME}
+    docker image push  ${REPO_NAME} 
+    echo "pushing ${REPO_NAME}"
+  done
+}
 
-if [ "$#" -ne 2 ]; then
-    echo "Incorrect parameters"
-    echo "Usage: build-services.sh <version> <prefix>"
-    exit 1
-fi
+function build_images {
 
-VERSION=$1
-PREFIX=$2
-SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+  if [ "$#" -ne 1 ]; then
+      echo "Incorrect parameters"
+      echo "Usage: build-services.sh <version>"
+      return 1
+  fi
 
-pushd "$SCRIPTDIR/productpage"
-  docker build --pull -t "${PREFIX}/examples-bookinfo-productpage-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-productpage-v1:latest" .
-  #flooding
-  docker build --pull -t "${PREFIX}/examples-bookinfo-productpage-v-flooding:${VERSION}" -t "${PREFIX}/examples-bookinfo-productpage-v-flooding:latest" --build-arg flood_factor=100 .
-popd
+  VERSION=$1
+  SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-pushd "$SCRIPTDIR/details"
-  #plain build -- no calling external book service to fetch topics
-  docker build --pull -t "${PREFIX}/examples-bookinfo-details-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-details-v1:latest" --build-arg service_version=v1 .
-  #with calling external book service to fetch topic for the book
-  docker build --pull -t "${PREFIX}/examples-bookinfo-details-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-details-v2:latest" --build-arg service_version=v2 \
-	 --build-arg enable_external_book_service=true .
-popd
-
-pushd "$SCRIPTDIR/reviews"
-  #java build the app.
-  docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
-  pushd reviews-wlpcfg
-    #plain build -- no ratings
-    docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v1:latest" --build-arg service_version=v1 .
-    #with ratings black stars
-    docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v2:latest" --build-arg service_version=v2 \
-	   --build-arg enable_ratings=true .
-    #with ratings red stars
-    docker build --pull -t "${PREFIX}/examples-bookinfo-reviews-v3:${VERSION}" -t "${PREFIX}/examples-bookinfo-reviews-v3:latest" --build-arg service_version=v3 \
-	   --build-arg enable_ratings=true --build-arg star_color=red .
+  pushd "$SCRIPTDIR/productpage"
+    docker build --pull -t "${PREFIX}/examples-bookinfo-productpage:v1-${VERSION}" .
+    #flooding
+    docker build --pull -t "${PREFIX}/examples-bookinfo-productpage:v-flooding-${VERSION}" --build-arg flood_factor=100 .
+    docker push "${PREFIX}/examples-bookinfo-productpage" --all-tags
   popd
-popd
 
-pushd "$SCRIPTDIR/ratings"
-  docker build --pull -t "${PREFIX}/examples-bookinfo-ratings-v1:${VERSION}" -t "${PREFIX}/examples-bookinfo-ratings-v1:latest" --build-arg service_version=v1 .
-  docker build --pull -t "${PREFIX}/examples-bookinfo-ratings-v2:${VERSION}" -t "${PREFIX}/examples-bookinfo-ratings-v2:latest" --build-arg service_version=v2 .
-  docker build --pull -t "${PREFIX}/examples-bookinfo-ratings-v-faulty:${VERSION}" -t "${PREFIX}/examples-bookinfo-ratings-v-faulty:latest" --build-arg service_version=v-faulty .
-  docker build --pull -t "${PREFIX}/examples-bookinfo-ratings-v-delayed:${VERSION}" -t "${PREFIX}/examples-bookinfo-ratings-v-delayed:latest" --build-arg service_version=v-delayed .
-  docker build --pull -t "${PREFIX}/examples-bookinfo-ratings-v-unavailable:${VERSION}" -t "${PREFIX}/examples-bookinfo-ratings-v-unavailable:latest" --build-arg service_version=v-unavailable .
-  docker build --pull -t "${PREFIX}/examples-bookinfo-ratings-v-unhealthy:${VERSION}" -t "${PREFIX}/examples-bookinfo-ratings-v-unhealthy:latest" --build-arg service_version=v-unhealthy .
-popd
+  pushd "$SCRIPTDIR/details"
+    #plain build -- no calling external book service to fetch topics
+    docker build --pull -t "${PREFIX}/examples-bookinfo-details:v1-${VERSION}"  --build-arg service_version=v1 .
+    #with calling external book service to fetch topic for the book
+    docker build --pull -t "${PREFIX}/examples-bookinfo-details:v2-${VERSION}"  --build-arg service_version=v2 \
+	   --build-arg enable_external_book_service=true .
+  popd
 
-pushd "$SCRIPTDIR/mysql"
-  docker build --pull -t "${PREFIX}/examples-bookinfo-mysqldb:${VERSION}" -t "${PREFIX}/examples-bookinfo-mysqldb:latest" .
-popd
+  pushd "$SCRIPTDIR/reviews"
+    #java build the app.
+    docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
+    pushd reviews-wlpcfg
+      #plain build -- no ratings
+      docker build --pull -t "${PREFIX}/examples-bookinfo-reviews:v1-${VERSION}" --build-arg service_version=v1 .
+      #with ratings black stars
+      docker build --pull -t "${PREFIX}/examples-bookinfo-reviews:v2-${VERSION}" --build-arg service_version=v2 \
+	     --build-arg enable_ratings=true .
+      #with ratings red stars
+      docker build --pull -t "${PREFIX}/examples-bookinfo-reviews:v3-${VERSION}" --build-arg service_version=v3 \
+	     --build-arg enable_ratings=true --build-arg star_color=red .
+    popd
+  popd
 
-pushd "$SCRIPTDIR/mongodb"
-  docker build --pull -t "${PREFIX}/examples-bookinfo-mongodb:${VERSION}" -t "${PREFIX}/examples-bookinfo-mongodb:latest" .
-popd
+  pushd "$SCRIPTDIR/ratings"
+    docker build --pull -t "${PREFIX}/examples-bookinfo-ratings:v1-${VERSION}" --build-arg service_version=v1 .
+    docker build --pull -t "${PREFIX}/examples-bookinfo-ratings:v2-${VERSION}" --build-arg service_version=v2 .
+    docker build --pull -t "${PREFIX}/examples-bookinfo-ratings:v-faulty-${VERSION}" --build-arg service_version=v-faulty .
+    docker build --pull -t "${PREFIX}/examples-bookinfo-ratings:v-delayed-${VERSION}" --build-arg service_version=v-delayed .
+    docker build --pull -t "${PREFIX}/examples-bookinfo-ratings:v-unavailable-${VERSION}" --build-arg service_version=v-unavailable .
+    docker build --pull -t "${PREFIX}/examples-bookinfo-ratings:v-unhealthy-${VERSION}" --build-arg service_version=v-unhealthy .
+  popd
+
+  pushd "$SCRIPTDIR/mysql"
+    docker build --pull -t "${PREFIX}/examples-bookinfo-mysqldb:${VERSION}" -t "${PREFIX}/examples-bookinfo-mysqldb:latest" .
+  popd
+
+  pushd "$SCRIPTDIR/mongodb"
+    docker build --pull -t "${PREFIX}/examples-bookinfo-mongodb:${VERSION}" -t "${PREFIX}/examples-bookinfo-mongodb:latest" .
+  popd
+}
+
+function create_repo_ecr {
+  for SERVICE in productpage details reviews ratings mysql mongodb
+  do
+    REPO_NAME="examples-bookinfo-${SERVICE}"
+    echo ${REPO_NAME}
+    aws ecr create-repository --repository-name ${REPO_NAME} 
+  done
+}
+
+function delete_repo_ecr {
+  for SERVICE in productpage details reviews ratings mysql mongodb
+  do
+    REPO_NAME="example-bookinfo-${SERVICE}"
+    echo ${REPO_NAME}
+    aws ecr delete-repository --repository-name ${REPO_NAME}
+  done
+}
